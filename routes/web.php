ed30baf85702bd4b3feee4c771703c88b05ee0b3
MelleVM <45255788+MelleVM@users.noreply.github.com>
2020-12-09 00:41:23 +0100

    Changed Stuff

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,18 @@ Route::view('/{path?}', ['app']);
 Route::view('/{path?}/{path2?}', ['app']);
 Route::view('/{path?}/{path2?}/{path3?}', ['app']);
 Route::view('/{path?}/{path2?}/{path3?}/{path4?}', ['app']);
+
+Route::get('/insert-json-file-to-database-table', function(){
+	$json = file_get_contents(storage_path('/app/public/images/db.json'));
+	$objs = json_decode($json,true);
+	foreach ($objs as $obj)  {	
+		foreach ($obj as $key => $value) {
+			if($key == "fields") {
+				$value['coordinates'] = implode(', ', $value['coordinates']);
+				$insertArr[str_slug($key,'_')] = $value;
+			}
+		}
+		DB::table('products')->insert($insertArr);
+	}
+	dd("Finished adding data in drinks table");
+});
