d89f414e6f769079867c4bca5e36cd72b115c858
MelleVM <45255788+MelleVM@users.noreply.github.com>
2020-12-09 00:42:24 +0100

    Changed Stuff

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/{path?}', ['app']);
-Route::view('/{path?}/{path2?}', ['app']);
-Route::view('/{path?}/{path2?}/{path3?}', ['app']);
-Route::view('/{path?}/{path2?}/{path3?}/{path4?}', ['app']);
-
 Route::get('/insert-json-file-to-database-table', function(){
 	$json = file_get_contents(storage_path('/app/public/images/db.json'));
 	$objs = json_decode($json,true);
@@ -21,3 +16,8 @@ Route::get('/insert-json-file-to-database-table', function(){
 	}
 	dd("Finished adding data in drinks table");
 });
+
+Route::view('/{path?}', ['app']);
+Route::view('/{path?}/{path2?}', ['app']);
+Route::view('/{path?}/{path2?}/{path3?}', ['app']);
+Route::view('/{path?}/{path2?}/{path3?}/{path4?}', ['app']);
