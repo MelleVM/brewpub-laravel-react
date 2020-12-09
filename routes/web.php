62e0239340ec58910ded4174a58548a6e820c6d7
MelleVM <45255788+MelleVM@users.noreply.github.com>
2020-12-09 11:08:10 +0100

    changed stuff

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/insert-json-file-to-database-table', function(){
-	$json = file_get_contents('https://mysterious-inlet-16244.herokuapp.com/storage/images/db.json');
+	$json = file_get_contents('https://data.opendatasoft.com/explore/dataset/open-beer-database@public-us/download/?format=json&refine.name_breweries=Rogue+Ales&refine.country=United+States&timezone=Europe/Berlin');
 	$objs = json_decode($json,true);
 	foreach ($objs as $obj)  {	
 		foreach ($obj as $key => $value) {
